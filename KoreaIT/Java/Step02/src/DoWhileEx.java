e3576c411f72c7102722cac9d49363b17edd0966
isthisthecar <cksgml7715@gmail.com>
2021-06-23 20:52:10 +0900

    0622 / Do while 예제

---
+++ KoreaIT/Java/Step02/src/DoWhileEx.java
@@ -0,0 +1,15 @@
+
+public class DoWhileEx {
+
+	public static void main(String[] args) {
+		// 일을 한 후 조건식 비교
+		// 처음 조건이 false이더라도 한번은 실행한다.
+		int i = 0;
+		
+		do {
+			System.out.println("Hello World");
+			i++;
+		}while(i<5);
+	}
+
+}
