f5a6e18f59c024c388fe403b09b3f82252a11412
isthisthecar <cksgml7715@gmail.com>
2021-06-23 20:59:24 +0900

    0623 / 2차원 배열 ㄹ채우기

---
+++ KoreaIT/Java/Step03/src/Array2DQuest02.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array2DQuest02 {
+
+	public static void main(String[] args) {
+		char[][] arr = new char[5][5];
+		
+		//길이 출력
+//		System.out.println(arr.length);//5 - 행 개수
+//		System.out.println(arr[0].length);//5 - 각 행의 열 개수
+//		System.out.println(arr[1].length);//5
+//		System.out.println(arr[2].length);//5
+//		System.out.println(arr[3].length);//5
+		
+		//값 저장
+		Scanner sc = new Scanner(System.in);
+		char ch = 'A'; // 알파벳도 숫자 -> 아스키코드
+		for(int i=0;i<arr.length;i++) {
+			if(i==0 || i == 2 || i == 4) {
+				for(int j = 0; j<arr[i].length;j++) {
+				arr[i][j] = ch;
+				ch++; // 알파벳도 숫자로 보기 때문에 +1 가능
+				}
+			}else
+				for(int j = 4; j>=0;j--) {
+					arr[i][j] = ch;
+					ch++;
+			}
+		}
+		
+		for(int i=0;i<arr.length;i++) {
+//			for(int j = 0; j<arr[i].length;j++) {
+//				System.out.print(arr[i][j] + " ");
+//			}
+//			System.out.println();
+			System.out.println(Arrays.toString(arr[i]));
+		}
+		
+	}
+
+}
