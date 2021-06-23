35b78f3689dda75c3d46dd20af7ae198b59c7c6d
isthisthecar <cksgml7715@gmail.com>
2021-06-23 21:01:32 +0900

    0623 / 소수 출력 문제

---
+++ KoreaIT/Java/Step03/src/Quest3.java
@@ -0,0 +1,21 @@
+
+public class Quest3 {
+
+	public static void main(String[] args) {
+		/*
+		 * 1~100까지의 숫자들 중
+		 * 소수만 출력
+		 * 
+		 */
+		for(int i=1;i<=100;i++) {
+			int j; // 나누는 숫자 2~자기자신 전까지
+			for(j=2; j<i;j++) {
+				if(i%j==0)
+					break;
+			}
+			if(i == j) System.out.print(i + " ");
+		}
+		
+	}
+
+}
