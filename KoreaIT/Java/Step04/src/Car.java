a3213c26fbef0abe920d2ee7e055c5ed1bab0654
isthisthecar <cksgml7715@gmail.com>
2021-06-24 19:04:00 +0900

    0624 / 간단한 클래스 연습

---
+++ KoreaIT/Java/Step04/src/Car.java
@@ -0,0 +1,24 @@
+
+public class Car {
+	// field 
+	int speed;
+	int gear;
+	boolean power;
+	boolean wiper;
+	boolean light;
+	double fuel;
+	final int MAX_SPEED = 300;
+	
+	
+	public Car(int speed, int gear, boolean power, boolean wiper, boolean light, double fuel) {
+		super();
+		this.speed = speed;
+		this.gear = gear;
+		this.power = power;
+		this.wiper = wiper;
+		this.light = light;
+		this.fuel = fuel;
+	}
+	
+	
+}
---
+++ KoreaIT/Java/Step04/src/Clothes.java
@@ -0,0 +1,43 @@
+
+public class Clothes {
+	//브랜드
+	String brand;
+	//제품명
+	String product;
+	//종류
+	int kind;
+	//계절
+	int season;
+	//성별
+	boolean gender;
+	//사이즈
+	int size;
+	//색상
+	String color;
+	//금액
+	int price;
+	//재고
+	int ea;
+	
+	
+	public Clothes(String brand, String product, int kind, int season, boolean gender, int size, String color,
+			int price, int ea) {
+		super();
+		this.brand = brand;
+		this.product = product;
+		this.kind = kind;
+		this.season = season;
+		this.gender = gender;
+		this.size = size;
+		this.color = color;
+		this.price = price;
+		this.ea = ea;
+	}
+	
+	
+	
+}
+
+
+
+
