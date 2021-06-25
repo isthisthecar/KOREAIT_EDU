fa6afb18c8fc20fec047b5a2410969279af82906
isthisthecar <cksgml7715@gmail.com>
2021-06-25 09:21:13 +0900

    0625 / Car 클래스

--- KoreaIT/Java/Step04/src/Car.java
+++ KoreaIT/Java/Step04/src/Car.java
@@ -19,6 +19,50 @@ public class Car {
 		this.light = light;
 		this.fuel = fuel;
 	}
+	public void powerOnOff() {
+		power = !power;
+		System.out.println(power ? "시동을 걸었습니다." : "시동을 껐습니다." );
+	}
 	
+	public void changeGear() {
+		if(gear < 6)
+			gear++;
+		switch(gear) {
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+			System.out.println(gear + "단으로 변경");
+			break;
+		case 0:
+			System.out.println("중립기어로 변경");
+			break;
+		case -1:
+			System.out.println("R단으로 변경");
+			break;
+		case -2:
+			System.out.println("P로 변경");
+			break;
+		
+		}
+	}
+	
+	public void accelator() {
+		if(gear == 0 && gear == -2)
+			return;
+		int val = gear == -1 ? -gear : gear;
+		if(speed < MAX_SPEED * ((double)val/6))
+			speed +=10;
+		System.out.println("현재속도 : " + speed);
+		
+	}
+	public void breakPush() {
+		speed -= 5;
+		if(speed <0)
+			speed = 0;
+		System.out.println("현재속도 : "+ speed);
+	}
 	
 }
