807bfaf5a9cbeb49208c40164271192de922f81c
isthisthecar <cksgml7715@gmail.com>
2021-06-28 21:10:42 +0900

    0628 / 인터페이스 예제

---
+++ KoreaIT/Java/Step06/src/inter/YoutubePlayer.java
@@ -0,0 +1,27 @@
+package inter;
+
+public class YoutubePlayer implements Player, Share{
+
+	@Override
+	public void play() {
+		System.out.println("유튜브 동영상 재생");
+	}
+	@Override
+	public void stop() {
+		System.out.println("유튜브 동영상 정지");
+	}
+	@Override
+	public void next() {
+		System.out.println("유튜브 다음 추천 동영상 재생");
+	}
+
+	@Override
+	public void pre() {
+		System.out.println("유튜브 동영상 이전 목록 재생");
+	}
+	@Override
+	public void snsShare() {
+		System.out.println("인스타에 해당 유튜브 동영상 공유");
+	}
+
+}
