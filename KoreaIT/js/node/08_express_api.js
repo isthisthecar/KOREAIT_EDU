9a3a56cfacc8e19f5dd7e546c0927e421333fb66
isthisthecar <cksgml7715@gmail.com>
2021-08-23 20:10:59 +0900

    CORS 오류 처리

---
+++ KoreaIT/js/node/08_express_api.js
@@ -0,0 +1,28 @@
+  
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.listen(port,()=>{
+    console.log(`서버 오픈 완료, 포트번호 : ${port}`);
+});
+//CORS 오류 처리
+app.all("/*",function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","X-Requested-With");
+    next();
+});
+
+app.get("/sendDate", (req, res) => {
+    var now = new Date();
+    var data = {};
+    data['year'] = now.getFullYear();
+    data['month'] = now.getMonth();
+    data['day'] = now.getDate();
+    data['hour'] = now.getHours();
+    data['minute'] = now.getMinutes();
+    data['second'] = now.getSeconds();
+    data['date'] = now.toLocaleString();
+    res.send(data);
+});
