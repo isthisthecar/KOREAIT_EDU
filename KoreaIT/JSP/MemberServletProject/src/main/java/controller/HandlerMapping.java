0833fe7dd701fc7c7547e2ccfb5e03a97bf7cc09
isthisthecar <cksgml7715@gmail.com>
2021-08-29 18:13:25 +0900

    컨트롤 / 핸들러맵핑

---
+++ KoreaIT/JSP/MemberServletProject/src/main/java/controller/HandlerMapping.java
@@ -0,0 +1,31 @@
+package controller;
+
+public class HandlerMapping {
+	private static HandlerMapping instance = new HandlerMapping();
+
+	private HandlerMapping() {
+	}
+
+	public static HandlerMapping getInstance() {
+		if(instance == null)
+			instance = new HandlerMapping();
+		return instance;
+	}
+	
+	public Controller createController(String command) {
+		Controller controller = null;
+		
+		switch(command) {
+		case "login.action":
+			controller = new LoginController();
+			break;
+		case "logout.action":
+			controller = new LogoutController();
+			break;
+		
+		}
+		
+		return controller;
+	}
+
+}
